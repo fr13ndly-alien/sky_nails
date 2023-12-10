a1828b5e01d38198cd9f1877152d8a0493ab6350
HenryLee <lhh@lineavn.com>
2023-12-10 07:41:55 +0700

    github-page

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjyP4siUPEScX-_RoRZ6OpExasarTxZxE",
+  authDomain: "skynails-henry.firebaseapp.com",
+  projectId: "skynails-henry",
+  storageBucket: "skynails-henry.appspot.com",
+  messagingSenderId: "896253582120",
+  appId: "1:896253582120:web:281b2e26e58ebaeda40e1d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
