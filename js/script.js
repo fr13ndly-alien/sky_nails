43b28943319d82b556bfbf3b4666fb7e2894ba83
Henry Le <henry.lee@extremereach.com>
2024-06-02 11:26:00 +0700

    disable promotion

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+const ENABLE_POPUP = false;
 const promotionPopup = document.getElementById('promotion-popup');
 const closeButton = promotionPopup.querySelector('.close-button');
 
-// Show the popup initially (set timeout for visual appeal if desired)
-promotionPopup.style.opacity = 1; // Or: setTimeout(() => promotionPopup.style.opacity = 1, 1000);
+if (ENABLE_POPUP) {
+    // Show the popup initially (set timeout for visual appeal if desired)
+    promotionPopup.style.opacity = 1; // Or: setTimeout(() => promotionPopup.style.opacity = 1, 1000);
 
-closeButton.addEventListener('click', () => {
-    promotionPopup.style.opacity = 0;
-});
-
-// Optional: Close the popup if the user clicks outside of it
-document.addEventListener('click', (event) => {
-    if (event.target === promotionPopup) {
+    closeButton.addEventListener('click', () => {
         promotionPopup.style.opacity = 0;
-    }
-});
+    });
+
+    // Optional: Close the popup if the user clicks outside of it
+    document.addEventListener('click', (event) => {
+        if (event.target === promotionPopup) {
+            promotionPopup.style.opacity = 0;
+        }
+    });
+} else {
+    promotionPopup.disabled = true;
+}
